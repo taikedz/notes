aa2e93bd4b15e414a9e26b42511f0777724d3cb5
Tai Kedzierski <dch.tai@gmail.com>
2017-11-10 17:28:54 +0000

    some initial ldap isntall notes

---
+++ ldap.sh
@@ -0,0 +1,9 @@
+apt install slapd phpldapadmin
+
+dpkg reconfigure phpldapadmin
+
+# need to edit /etc/phpldapadmin (or similar)
+# review https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-a-basic-ldap-server-on-an-ubuntu-12-04-vps
+
+sensible-browser http://localhost/phpldapadmin
+
