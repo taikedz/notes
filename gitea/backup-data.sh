60e0c711f901f8a45309d1e1b59c70fdda0a698a
Tai Kedzierski <dch.tai gmail>
2018-06-09 15:58:44 +0100

    Adjusted for full operation

--- gitea/backup-data.sh
+++ gitea/backup-data.sh
@@ -1,6 +1,19 @@
+archivef="$(basename "${1:-gitea-data.tgz}")"
+
+if [[ -f "./$archivef" ]]; then
+	read -p "Overwrite ./$archivef ? y/N> "
+	[[ "$REPLY" =~ ^(y|Y|yes|YES)$ ]] || {
+		echo Abort
+		exit 1
+	}
+	mv "$archivef" "${archivef}-old"
+fi
+
 docker run --rm \
     -v "$PWD":/backup \
     -v gitea_gitea:/data \
     -v gitea_mysql:/var/lib/mysql \
     alpine \
-    tar czf /backup/gitea-data.tgz /data /var/lib/mysql
+    tar czf "/backup/$archivef" /data /var/lib/mysql
+
+[[ -f "${archivef-old}" ]] && rm "${archivef}-old" -f
--- gitea/restore-data.sh
+++ gitea/restore-data.sh
@@ -1,6 +1,22 @@
+# In order to use this properly:
+#
+# 	docker-compose down
+# 	docker volume rm gitea_gitea gitea_mysql
+#
+# 	docker-compose up --no-start
+# 	./restore.sh [ARCHVIEFILE]
+# 	docker-compose up -d
+
+archivef="$(basename "${1:-gitea-data.tgz}")"
+
+[[ -f "$archivef" ]] || {
+	echo "No such file ./$archivef"
+	exit 1
+}
+
 docker run --rm \
     -v "$PWD":/backup \
     -v gitea_gitea:/data \
     -v gitea_mysql:/var/lib/mysql \
     alpine \
-    tar xzf -C / /backup/gitea-data.tgz
+    tar xz -C / -f "/backup/$archivef"
